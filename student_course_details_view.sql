5ba56c7cada5d39c3b65dec6635d05099bc34f8c
KaterinaLatushka <149284345+KaterinaLatushka@users.noreply.github.com>
2026-03-01 21:38:08 -0500

    student course details view added

--- student_course_details_view.sql
+++ student_course_details_view.sql
@@ -1,1 +1,13 @@
-
+CREATE VIEW student_course_details AS
+SELECT
+  s.student_id,
+  s.student_name,
+  s.major,
+  c.course_id,
+  c.course_name,
+  c.credits,
+  e.semester,
+  e.grade
+FROM students AS s
+JOIN enrollments AS e ON s.student_id = e.student_id
+JOIN courses AS c ON e.course_id = c.course_id;
